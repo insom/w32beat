29d5062cfb60703961259d18f4a6ccab28a5f8f3
Aaron Brady <aaron@insom.me.uk>
2023-10-09 14:20:16 -0400

    Make it an actual clock

--- src/main.c
+++ src/main.c
@@ -4,18 +4,41 @@
 HWND label;
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    SYSTEMTIME st;
+
     switch (uMsg) {
         case WM_CREATE:
-            SetWindowPos(hwnd, NULL, 0, 0, 300, 300, SWP_NOMOVE);
+            SetWindowPos(hwnd, NULL, 0, 0, 100, 100, SWP_NOMOVE);
             break;
         case WM_SIZE:
             WORD w = LOWORD(lParam);
             WORD h = HIWORD(lParam);
-            SetWindowText(label, L"Hello, world!");
             MoveWindow(label, 0, 0, w, h, 1);
             break;
         case WM_CLOSE:
             PostQuitMessage(0);
+            break;
+        case WM_TIMER:
+            GetSystemTime(&st);
+            LONG beatSeconds = 10 * (3600 + (st.wHour * 3600) +
+                                     (st.wMinute * 60) + st.wSecond);
+            beatSeconds %= 864000;
+            DOUBLE beats = beatSeconds / 86.4;
+
+            /* Hey, Aaron -- isn't this nuts?
+
+            Well, these are the lengths I will travel to avoid importing the
+            MSVC C runtime libraries, and therefore make this a self-contained
+            pure Win32 binary. */
+
+            WCHAR buf[7] = {L'@'};
+
+            buf[1] = L'0' + ((WORD)(beats / 1000) % 10);
+            buf[2] = L'0' + ((WORD)(beats / 100) % 10);
+            buf[3] = L'0' + ((WORD)(beats / 10) % 10);
+            buf[4] = L'.';
+            buf[5] = L'0' + ((WORD)beats % 10);
+            SetWindowText(label, (LPCWSTR)(&buf));
             break;
     }
     return DefWindowProc(hwnd, uMsg, wParam, lParam);
@@ -31,10 +54,13 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
     wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
     RegisterClass(&wc);
 
-    HWND hwnd = CreateWindowEx(WS_EX_CLIENTEDGE, L"INSOM_WNDCLASS", L"INSOM",
-                               WS_OVERLAPPEDWINDOW, CW_USEDEFAULT,
-                               CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-                               NULL, NULL, hInstance, NULL);
+    SetProcessDPIAware();
+
+    HWND hwnd = CreateWindowEx(
+        WS_EX_CLIENTEDGE | WS_EX_NOACTIVATE | WS_EX_APPWINDOW | WS_EX_TOPMOST,
+        L"INSOM_WNDCLASS", L".beat Clock", WS_OVERLAPPED | WS_BORDER,
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL,
+        hInstance, NULL);
     if (hwnd == NULL) return -1;
 
     label = CreateWindowEx(0, L"STATIC", L"IDK",
@@ -43,6 +69,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
     if (label == NULL) return -1;
 
     ShowWindow(hwnd, nCmdShow);
+    SetTimer(hwnd, 10522, 100, NULL);
 
     MSG msg = {0};
     while (GetMessage(&msg, NULL, 0, 0)) {
@@ -51,6 +78,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
             DispatchMessage(&msg);
         }
     }
+
+    KillTimer(hwnd, 10522);
 
     return 0;
 }
